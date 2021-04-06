7c3dc17a484e1b554f2afd5da49cbcf2d423720e
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-04-06 18:27:49 -0300

    Distribuição automatica das cartas sem gif + Mobile

--- Script/script.js
+++ Script/script.js
@@ -1,17 +1,18 @@
 let Numero_Cartas = prompt("Digite o número de cartas do jogo:");
 
 while (Numero_Cartas < 4 || Numero_Cartas > 14 || Numero_Cartas%2 !== 0) {
-    const obs = "Obs: Devem ser escolhindo números pares entre 4 e 14"
+    const obs = "Obs: Deve ser escolhindo número par entre 4 e 14"
     Numero_Cartas = prompt(`Digite o número de cartas do jogo:\n${obs}`);
 }
 
-const lista_cartas = [];
+const lista_gifs = [];
+lista_gifs.push("<img src='Img-GIF/bobrossparrot.gif' alt='Parrot'>")
 
-/**for (let i = 0; i < Numero_Cartas; i++) {
-    const elemento = document.querySelector(".container_Cartas");
+for (let i = 0; i < Numero_Cartas; i++) {
+    const elemento = document.querySelector("ul");
     elemento.innerHTML += `
     <li class="cartas">
-    <img src="Img-GIF\front.png" alt="Parrot">
+    <img src="Img-GIF/front.png" alt="Parrot">
     </li>`
-}**/
+}
 
